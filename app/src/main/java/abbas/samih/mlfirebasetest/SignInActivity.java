46aaf362317d25ff17e35a6aaa8abb1560e37b5d
doniataskman <doniahammud@gmail.com>
2022-03-22 12:18:52 +0200

    Initial commit

---
+++ app/src/main/java/abbas/samih/mlfirebasetest/SignInActivity.java
@@ -0,0 +1,139 @@
+package abbas.samih.mlfirebasetest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import abbas.samih.mlfirebasetest.FullExample.BaseActivity;
+
+public class SignInActivity extends AppCompatActivity {
+    private TextInputEditText etEmail, etPassword, etRepassword, etPhone, etFullName;
+    private Button btnSave;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        etRepassword = findViewById(R.id.etRePassword);
+        etPhone = findViewById(R.id.etPhone);
+        btnSave = findViewById(R.id.btnSave);
+        etFullName = findViewById(R.id.etFullName);
+
+              /**
+                *
+                */
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate();
+            }
+
+            private void validate()
+                {
+                    boolean isOk = true;
+                    String email = etEmail.getText().toString();
+                    String password = etPassword.getText().toString();
+                    // فحص هل طول الايميل يساوي 8
+                    if (email.length() == 8) {
+                        etEmail.setError("enter email");
+                    }
+                    if(password.length()<8)
+                    {
+                        etPassword.setError("password at least 8 letters");
+                    }
+
+                    if (isOk)
+                    {
+                        signingin(email,password);
+                    }
+                }
+
+            private void signingin(String email, String passw) {
+                FirebaseAuth auth = FirebaseAuth.getInstance();
+                auth.signInWithEmailAndPassword(email, passw).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Signing in successful", Toast.LENGTH_SHORT).show();
+                            finish();
+                            startActivity(new Intent(getApplicationContext(), BaseActivity.class));
+                        } else
+                            {
+                            Toast.makeText(getApplicationContext(), "signing in error:" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+
+        });
+            boolean isOk = true;
+            String email = etEmail.getText().toString();
+            String password = etPassword.getText().toString();
+            String rePassword = etRepassword.getText().toString();
+            String phone = etPhone.getText().toString();
+            String fullName = etFullName.getText().toString();
+
+            if(email.length()<5 || email.indexOf('@')<=0)
+            {
+                etEmail.setError("wrong email syntax");
+                isOk=false;
+            }
+            // فحص طول كلمة السر
+            if(password.length()<8)
+            {
+                etPassword.setError("at least 8");
+                isOk=false;
+            }
+            //  فحص اذا  كلمة سر (repassword) تساوي كلمة السر (password)
+            if(password.equals(rePassword)==false)
+            {
+                etPassword.setError("not equal passwords");
+                isOk=false;
+            }
+            // فحص اذا طول الاسم الكامل يساوي 8
+            if(fullName.length()==0)
+            {
+                etFullName.setError("must to enter full name");
+                isOk=false;
+            }
+            if(isOk)
+            {
+                CreateAccount(email,password);
+            }
+        }
+
+        private void CreateAccount(String email, String password)
+        {
+            FirebaseAuth auth=FirebaseAuth.getInstance();
+            //request                                                 //wait for response
+            auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                //response handler
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful() == true) {
+                        finish();
+                        startActivity(new Intent(getApplicationContext(), BaseActivity.class));
+                    } else {
+                        //dialog
+                        Toast.makeText(getApplicationContext(), "error create account" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            });
+        }
+}
+
+
---
+++ app/src/main/java/abbas/samih/mlfirebasetest/logInActivity.java
@@ -0,0 +1,26 @@
+package abbas.samih.mlfirebasetest;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class logInActivity extends AppCompatActivity
+
+{
+    private TextInputEditText etEmail,etpassword;
+    private Button btnLogIn,btnRegister;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+        etEmail=findViewById(R.id. etEmail);
+        etpassword=findViewById(R.id. etpassword);
+        btnLogIn=findViewById(R.id. btnLogIn);
+        btnRegister=findViewById(R.id. btnRegister);
+
+    }
+}
